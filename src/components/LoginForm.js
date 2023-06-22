e857cc03170d24fb1f4b5baef775e083742e72f9
Jodi-Ann Moore-Barrett <moorebarrett.jodiann@gmail.com>
2023-06-22 14:17:31 -0500

    fix merge conflicts with login form

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,98 +1,92 @@
-import { 
-  logInWithEmailAndPassword,
-  logInWithGoogle,
-} from '../config/firebase'
+import { logInWithEmailAndPassword, logInWithGoogle } from '../config/firebase'
 import { useState } from 'react'; 
-import NiceModal, { useModal } from "@ebay/nice-modal-react";
+import { useModal } from "@ebay/nice-modal-react";
 import LoginModal from "./LoginModal";
 
 function LoginForm({handleToggleForm}) {
-  const [emailInput, setEmailInput] = useState('');
-  const [passwordInput, setPasswordInput] = useState('');
-  const [wasValidated, setWasValidated] = useState(false);
+	const [emailInput, setEmailInput] = useState('');
+	const [passwordInput, setPasswordInput] = useState('');
+	const [wasValidated, setWasValidated] = useState(false);
 
-  const modal = useModal(LoginModal);
-  const googleLogo = <i className="fa-brands fa-google"></i>;
+	const modal = useModal(LoginModal);
 
-  
-  const handleLogin = async () => {
-    try {
-      const res = await logInWithEmailAndPassword(emailInput, passwordInput);
-      if(res) {
-        modal.remove()
-      }
+	const handleLogin = async () => {
+		try {
+			const res = await logInWithEmailAndPassword(emailInput, passwordInput);
+		if(res) {
+			modal.remove()
+		}
 
-      setWasValidated(true);
-      
-    } catch(error) {
-      console.error(error);
-    }
-  }
+		setWasValidated(true);
+		
+		} catch(error) {
+			console.error(error);
+		}
+	}
 
 
-  const handleGoogleLogin = () => {
-    logInWithGoogle();
-    modal.remove();
-  }
+	const handleGoogleLogin = () => {
+		logInWithGoogle();
+		modal.remove();
+	}
 
-  return (
-      <div className="container p-2">
-        <form className="needs-validation" noValidate>
-          <div className="container d-flex flex-column">
-            <div className="form-floating mb-3 has-validation">
-              <input 
-                type="email" 
-                id="email" 
-                placeholder="Email"
-                className="form-control" 
-                value={emailInput} 
-                onChange={(e) => {setEmailInput(e.target.value)}}
-                required
-              />
-              <label className="form-label" htmlFor="email" >Email</label>
-            </div>
-            <div className="form-floating mb-3 form-input-box2">
-              <input 
-                type="password" 
-                id="password" 
-                placeholder="Password"
-                className="form-control" 
-                value={passwordInput} 
-                onChange={(e) => {setPasswordInput(e.target.value)}} 
-                required
-              />
-              <label className="form-label" htmlFor="password" > Password</label>
-            </div>
-            {wasValidated ? <small className='text-danger font-sm'>Username or password incorrect</small> : <></>}
-            <input
-              type="button"
-              value="Sign in"
-              className="btn btn-warning my-3"
-              onClick={handleLogin}
-            />
-            <p className='sign-in-option'>- or -</p>
-            <input
-              type="button"
-              value="Sign In with Google"
-              className="btn btn-link sign-in-google"
-              onClick={handleGoogleLogin}
-              data-bs-dismiss="modal"
-            />
-            <div className='register-option'>
-              <p>Don't have an account?</p>
-              <input
-                type="button"
-                value="Register"
-                className="btn btn-link"
-                onClick={handleToggleForm}
-                hidden={formType === "Register"}
-              />
-            </div>
-          </div>
-        </form>
-      </div>
-    </div>
-  )
+	return (
+		<div className="container p-2">
+			<form className="needs-validation" noValidate>
+				<div className="container d-flex flex-column">
+				<div className="form-floating mb-3 has-validation">
+					<input 
+						type="email" 
+						id="email" 
+						placeholder="Email"
+						className="form-control" 
+						value={emailInput} 
+						onChange={(e) => {setEmailInput(e.target.value)}}
+						required
+					/>
+					<label className="form-label" htmlFor="email" >Email</label>
+				</div>
+				<div className="form-floating mb-3 form-input-box2">
+					<input 
+						type="password" 
+						id="password" 
+						placeholder="Password"
+						className="form-control" 
+						value={passwordInput} 
+						onChange={(e) => {setPasswordInput(e.target.value)}} 
+						required
+					/>
+					<label className="form-label" htmlFor="password" > Password</label>
+				</div>
+				{wasValidated ? <small className='text-danger font-sm'>Username or password incorrect</small> : <></>}
+				<input
+					type="button"
+					value="Sign in"
+					className="btn btn-warning my-3"
+					onClick={handleLogin}
+				/>
+				<p className='sign-in-option'>- or -</p>
+				<input
+					type="button"
+					value="Sign In with Google"
+					className="btn btn-link sign-in-google"
+					onClick={handleGoogleLogin}
+					data-bs-dismiss="modal"
+				/>
+				<div className='register-option'>
+					<p>Don't have an account?</p>
+					<input
+					type="button"
+					value="Register"
+					className="btn btn-link"
+					onClick={handleToggleForm}
+					/>
+				</div>
+				</div>
+			</form>
+		</div>
+	)
+
 }
 
 export default LoginForm
